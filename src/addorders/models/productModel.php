d61a773428c27322d649a0d94356b4fd1a5d0843
mohamed mustafa <mohamedmustafa8112001@gmail.com>
2025-03-20 04:11:57 +0200

    Update

--- src/addorders/models/productModel.php
+++ src/addorders/models/productModel.php
@@ -1,33 +1,3 @@
-<<<<<<< HEAD
-<?php
-require_once __DIR__ . '/../includes/database.php';
-require_once __DIR__ . '/../entities/product.php';
-
-class ProductModel{
-    private $db;
-
-    public function __construct($conn){
-        $this->db=new Database($conn);
-    }
-
-    public function getAllProducts(){
-        $records=$this->db->select("select p.id as id, p.name as name, p.price as price,p.status as status, p.image_path as image_path, c.name as category from product as p, category as c where p.category_id=c.id;");
-        $products=[];
-        foreach($records as $record){
-            $products[] = new Product(
-                $record['id'],
-                $record['name'],
-                $record['price'],
-                $record['status'],
-                $record['image_path'],
-                $record['category'],
-            );
-        }
-        return $products;
-    }
-}
-
-=======
 <?php
 require_once __DIR__ . '/../includes/database.php';
 require_once __DIR__ . '/../entities/product.php';
@@ -57,5 +27,4 @@ class ProductModel{
     }
 }
 
->>>>>>> 37d04166494cee8a050963ebfdb3b7b3bf34a44d
 ?>
